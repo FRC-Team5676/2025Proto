c9d098dde53742de27d148e075acc3f35412c28e
Team 5676 <team5676@outlook.com>
2025-02-17 13:42:17 -0500

    Arm code

---
+++ src/main/java/frc/robot/subsystems/LowerArmSubsystem.java
@@ -0,0 +1,91 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.ShuffleboardContent;
+
+public class LowerArmSubsystem extends SubsystemBase {
+
+  public double rotations;
+
+  private final int m_lowerArmCanId = 51;
+
+  private final RelativeEncoder m_driveEncoder;
+  private final SparkMax m_driveMotor;
+  private final SparkClosedLoopController m_driveController;
+
+  private final double minRotations = -20;
+  private final double maxRotations = 90;
+
+  public LowerArmSubsystem() {
+    // Drive Motor setup
+    m_driveMotor = new SparkMax(m_lowerArmCanId, MotorType.kBrushless);
+
+    // drive encoder setup
+    m_driveEncoder = m_driveMotor.getEncoder();
+
+    m_driveController = m_driveMotor.getClosedLoopController();
+
+    ShuffleboardContent.initLowerArm(this);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  public void moveToPosition(double position) {
+    setReferenceValue(position);
+    setReferencePeriodic();
+  }
+
+  public void moveToFarPosition() {
+    setReferenceValue(maxRotations);
+    setReferencePeriodic();
+  }
+
+  public void moveToMidPosition() {
+    setReferenceValue(22);
+    setReferencePeriodic();
+  }
+
+  public void moveToBackPosition() {
+    setReferenceValue(-20);
+    setReferencePeriodic();
+  }
+
+  public double getMinRotations() {
+    return minRotations;
+  }
+
+  public double getMaxRotations() {
+    return maxRotations;
+  }
+
+  public double getPosition() {
+    return m_driveEncoder.getPosition();
+  }
+
+  public void driveArm(double throttle) {
+    throttle = -throttle;
+    rotations += throttle;
+    setReferencePeriodic();
+  }
+
+  public void stop() {
+    m_driveMotor.set(0);
+  }
+
+  public void setReferenceValue(double rotation) {
+    rotations = rotation;
+  }
+
+  public void setReferencePeriodic() {
+    rotations = MathUtil.clamp(rotations, minRotations, maxRotations);
+    m_driveController.setReference(rotations, SparkMax.ControlType.kPosition);
+  }
+}
---
+++ src/main/java/frc/robot/utils/ShuffleboardContent.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utils;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import frc.robot.subsystems.LowerArmSubsystem;
+
+/** Add your docs here. */
+public class ShuffleboardContent {
+
+        static ShuffleboardLayout boolsLayout;
+
+        public ShuffleboardContent() {
+
+        }
+
+        public static void initLowerArm(LowerArmSubsystem drive) {
+                ShuffleboardTab drLayout1 = Shuffleboard.getTab("Arms");
+
+                drLayout1.addNumber("Lower Arm Angle", () -> drive.getPosition())
+                        .withPosition(9, 0)
+                        .withSize(8, 1);
+                drLayout1.addNumber("Lower Current Rotations", () -> drive.rotations)
+                        .withPosition(9, 3)
+                        .withSize(8, 1);
+                drLayout1.addNumber("Lower Min Rotations", () -> drive.getMinRotations())
+                        .withPosition(9, 6)
+                        .withSize(8, 1);
+                drLayout1.addNumber("Lower Max Rotations", () -> drive.getMaxRotations())
+                        .withPosition(9, 9)
+                        .withSize(8, 1);
+        }
+
+}
