c4e3ffdea6215bfec8339f34672d794a720b43ab
Team5676 <team5676@outlook.com>
2025-02-26 16:36:57 -0500

    Fix combo button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -102,7 +102,9 @@ public class RobotContainer {
 
         // Move Arms
         operator.button(XboxController.Button.kX.value).onTrue(armCommands.moveToHome());
-        operator.povLeft().and(operator.button(XboxController.Button.kX.value).onTrue(armCommands.pickupCoral()));
+        operator.povLeft()
+            .and(operator.button(XboxController.Button.kX.value))
+            .onTrue(armCommands.pickupCoral());
         operator.button(XboxController.Button.kY.value).onTrue(armCommands.moveToL2());
 
         // Algea
